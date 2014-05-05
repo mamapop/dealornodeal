8cb673123c63299a84b765745268f74e6bcbe661
Brian Kaplan <bakaplan@vt.edu>
2014-05-05 00:20:55 -0400

    initial commit v0.0

---
+++ final_project.c
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+struct  Case
+{
+	double value;
+	string name;
+	bool inPlay;
+	
+};
+
+vector<Case> board;
+
+void Build()
+{
+	for (int i = 0; i < count; ++i)
+	{
+		/* code */
+	}
+}
+
+void shuffle()
+{
+
+}
+
+
+void removeCase()
+{
+
+}
+
+
+void displayBoard()
+{
+
+}
+
+
+
+void int main(int argc, char const *argv[])
+{	
+
+
+
+	return 0;
+}
